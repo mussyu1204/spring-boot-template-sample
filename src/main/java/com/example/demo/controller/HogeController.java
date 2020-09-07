6edc3eb665ae8a246d45cc2b98d38e3a0e6cdc4c
maimai1234 <maimai.5w@gmail.com>
2020-09-07 22:29:34 +0900

    test commit

---
+++ src/main/java/com/example/demo/controller/HogeController.java
@@ -0,0 +1,24 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HogeController {
+
+    @RequestMapping("/top")
+    public String topView(){
+        return "maimai/top";
+    }
+
+    @RequestMapping("/input1.html")
+    public String input1View(){
+        return "maimai/input1";
+    }
+
+    @RequestMapping("/input2.html")
+    public String input2View(){
+        return "maimai/top";
+    }
+
+}
