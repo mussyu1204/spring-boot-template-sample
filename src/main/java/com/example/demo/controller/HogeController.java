81e30ca0536b2f6c090ac3cdb10225013df1339c
shintaro mizunuma <shintaro.mizunuma@gmail.com>
2020-09-11 01:06:51 +0900

    入力フォームで値を保持、バインドエラー時はエラー画面に。

--- src/main/java/com/example/demo/controller/HogeController.java
+++ src/main/java/com/example/demo/controller/HogeController.java
@@ -5,6 +5,7 @@ import com.example.demo.domain.HogeMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -15,14 +16,21 @@ public class HogeController {
     HogeMapper hogeMapper;
 
     @RequestMapping("/top")
-    public String topView(){
-        return "maimai/top";
+    public ModelAndView topView(@ModelAttribute("hoge") Hoge hoge, ModelAndView mav ) {
+        mav.setViewName("maimai/top");
+        return mav;
     }
 
     @RequestMapping(value = "/complete", method = RequestMethod.POST)
-    public ModelAndView completeView(@ModelAttribute Hoge hoge, Model model) {
-        ModelAndView mav = new ModelAndView();
-
+    public ModelAndView completeView(@ModelAttribute Hoge hoge, BindingResult result, ModelAndView mav) {
+        // ModelAndView mav = new ModelAndView();
+        if (result.hasErrors()) {
+            // バインドエラー時
+            System.out.println("bind error");
+            mav.setViewName("error");
+            return mav;
+        }
+        System.out.println("bind success");
         mav.setViewName("maimai/complete");
         hogeMapper.insertHoge(hoge);
 
